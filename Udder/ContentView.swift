55c63b3cee99d5502921d70ae930cdf5d4ad6607
machimbu <62740266+machimbu@users.noreply.github.com>
2020-08-04 21:02:02 +0800

    Initial Commit

---
+++ Udder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by Alexis Conomos on 4/8/20.
+//  Copyright © 2020 Cosmo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
